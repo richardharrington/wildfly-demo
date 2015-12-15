ab36afd8bf57d5f4e12795d72946e0ffced40880
Richard Harrington <Richard.Harrington@mphasis.com>
2015-12-15 13:54:53 -0500

    first commit

---
+++ src/demo/core.clj
@@ -0,0 +1,20 @@
+(ns demo.core
+  (:require [immutant.web :as web]
+            [immutant.caching :as caching]
+            [immutant.messaging :refer :all]
+            [immutant.scheduling :as scheduling]
+            [immutant.transactions :as transactions]
+            [immutant.wildfly :as wildfly]
+            [clojure.pprint :refer (pprint)]
+            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+
+            )
+  (:gen-class))
+
+(defn app [request]
+  {:status 200
+   :body "Hello World!"})
+
+(defn -main
+  [& args]
+  (web/run app))
